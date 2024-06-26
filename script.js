958b96e9dc3a1a051cf1e4e53dde3be0c9c4c5e9
Monish.R <140420819+MonishRaman@users.noreply.github.com>
2024-06-26 11:36:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function bookAppointment() {
+    window.location.href = 'login.html'; // Replace with your login page URL
+}
+
+function instantVideoConsult() {
+    // You can add functionality for the instant video consult here
+    alert("Instant Video Consult feature is not implemented yet.");
+}
+function redirectToZoom(zoomLink) {
+    window.location.href = zoomLink;
+}
